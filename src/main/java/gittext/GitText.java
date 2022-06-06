48c41dbceaee6aaaf8fee9d814871afbae3ab66e
ChenJiwei <2823222711@qq.com>
2022-06-06 15:57:23 +0800

    second gittext

--- src/main/java/gittext/GitText.java
+++ src/main/java/gittext/GitText.java
@@ -3,5 +3,6 @@ package gittext;
 public class GitText {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello world");
     }
 }
